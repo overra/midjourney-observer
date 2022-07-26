d1cc4bf28ab32cfe28879041d4254c360a88ea0a
Adam Snodgrass <overra@gmail.com>
2022-07-26 11:21:35 -0500

    fix: also send payload?

--- routes/api/event.ts
+++ routes/api/event.ts
@@ -1,9 +1,13 @@
 import { HandlerContext } from "$fresh/server.ts";
 
-export const handler = (
+export const handler = async (
   _req: Request,
   _ctx: HandlerContext
 ): Promise<Response> => {
   const { url, ...rest } = _req;
-  return fetch("https://plausible.io/api/event", { ...rest, method: "post" });
+  return fetch("https://plausible.io/api/event", {
+    ...rest,
+    method: "post",
+    body: await _req.text(),
+  });
 };
