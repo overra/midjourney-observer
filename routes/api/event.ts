063907192c279f6d289461ff67dc55d31a5e153f
Adam Snodgrass <overra@gmail.com>
2022-07-26 12:55:04 -0500

    fix: try a more simple proxy

--- routes/api/event.ts
+++ routes/api/event.ts
@@ -5,19 +5,9 @@ export const handler = async (
   _ctx: HandlerContext
 ): Promise<Response> => {
   const addr = _ctx.remoteAddr as Deno.NetAddr;
-  const ip = addr.hostname;
-  const { url, ...rest } = _req;
 
-  return fetch("https://plausible.io/api/event", {
-    ...rest,
-    method: "post",
-    body: await _req.text(),
-    headers: {
-      // @ts-expect-error both mdn and deno have this
-      // https://deno.com/deploy/docs/runtime-headers#methods
-      // https://developer.mozilla.org/en-US/docs/Web/API/Headers/entries
-      ...Object.fromEntries(rest.headers.entries()),
-      "X-Forwarded-For": ip,
-    },
-  });
+  _req.headers.set("host", "plausible.io");
+  _req.headers.set("hostname", "https://plausible.io");
+  _req.headers.set("X-Forwarded-For", addr.hostname);
+  return await fetch(_req);
 };
