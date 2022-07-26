162581916ac7d52b3e24aab0ccad158975dfff60
Adam Snodgrass <overra@gmail.com>
2022-07-26 12:06:32 -0500

    fix: forward ip to plausible

--- routes/api/event.ts
+++ routes/api/event.ts
@@ -4,10 +4,17 @@ export const handler = async (
   _req: Request,
   _ctx: HandlerContext
 ): Promise<Response> => {
+  const addr = _ctx.remoteAddr as Deno.NetAddr;
+  const ip = addr.hostname;
   const { url, ...rest } = _req;
+
   return fetch("https://plausible.io/api/event", {
     ...rest,
     method: "post",
     body: await _req.text(),
+    headers: {
+      ...rest.headers,
+      "X-Forwarded-For": ip,
+    },
   });
 };
